3a094c6bf9730cad9cc53804760294034e88e825
DmitriyKovchugo <dkovchugo@mail.ru>
2023-11-30 16:21:10 +0300

    feat: add dynamic Map

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -1,10 +1,21 @@
 import { useState } from "react"
 import style from "./index.module.scss"
-import Modal from "./Modal";
+import Modal from "../Modal";
+import Map from "../Map";
 
 export const Schedule = () => {
 
    const [modalActive, setModalActive] = useState<boolean>(false);
+   const [mapCoordinates, setMapCoordinates] = useState<[number, number]>([53.964824, 25.124172]);
+   const [mapBalloon, setMapBalloon] = useState<string>("Гродненская область, Вороновский район");
+   const [key, setKey] = useState<number>(0)
+
+   const handleSpanClick = (coordinates: [number, number], balloon: string) => {
+      setMapCoordinates(coordinates);
+      setMapBalloon(balloon);
+      setModalActive(true);
+      setKey(prevKey => prevKey + 1); // Увеличиваем ключ, чтобы вызвать повторную отрисовку Map
+   }
 
    return (
       <>
@@ -14,45 +25,48 @@ export const Schedule = () => {
                   <h3>09:00</h3>
                </div>
                <div className={style.details}>
-                  <h3>Утро невесты</h3>
+                  <h3>Венчание</h3>
 
-                  <p>Фотосессия в отеле Radisson Collection Hotel</p>
+                  <p>Костёл Воздвижения Креста Господня</p>
                   <p className={style.address}>
-                     <span onClick={() => setModalActive(true)}>
-                        Россия, Москва, Певческий переулок, 4с</span>
+                     <span onClick={() => handleSpanClick([53.889263, 25.303251], "Лида, Советская ул.,2")}>
+                        Лида, Советская ул.,2</span>
                   </p>
                </div>
             </li>
             <li>
                <div className={style.time}>
-                  <h3>09:00</h3>
+                  <h3>13:00</h3>
                </div>
                <div className={style.details}>
-                  <h3>Утро невесты</h3>
+                  <h3>Выездная роспись</h3>
 
-                  <p>Фотосессия в отеле Radisson Collection Hotel</p>
+                  <p>Усадьба "Барыш"</p>
                   <p className={style.address}>
-                     <span>
-                        Россия, Москва, Певческий переулок, 4с</span>
+                     <span onClick={() => handleSpanClick([53.964824, 25.124172], "Гродненская область, Вороновский район")}>
+                        Гродненская область, Вороновский район
+                     </span>
                   </p>
                </div>
             </li>
             <li>
                <div className={style.time}>
-                  <h3>09:00</h3>
+                  <h3>18:00</h3>
                </div>
                <div className={style.details}>
-                  <h3>Утро невесты</h3>
+                  <h3>Праздничный ужин</h3>
 
-                  <p>Фотосессия в отеле Radisson Collection Hotel</p>
+                  <p>Усадьба "Барыш"</p>
                   <p className={style.address}>
-                     <span>
-                        Россия, Москва, Певческий переулок, 4с</span>
+                     <span onClick={() => handleSpanClick([53.964824, 25.124172], "Гродненская область, Вороновский район")}>
+                        Гродненская область, Вороновский район
+                     </span>
                   </p>
                </div>
             </li>
          </ul>
          <Modal active={modalActive} setActive={setModalActive}>
+            <Map coordinates={mapCoordinates} balloon={mapBalloon} key={key} />
          </Modal>
       </>
 
