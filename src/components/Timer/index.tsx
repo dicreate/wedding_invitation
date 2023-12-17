b31a94f201c6aee10a7e9670ea3f1b9f179646f7
DmitriyKovchugo <dkovchugo@mail.ru>
2023-12-17 13:35:56 +0300

    fix: fix warning on Timer

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import style from './index.module.scss'
 
 const Timer = () => {
@@ -7,61 +7,63 @@ const Timer = () => {
    const [timerMinutes, setTimerMinutes] = useState<string>('00');
    const [timerSeconds, setTimerSeconds] = useState<string>('00');
 
-   let interval = useRef();
-
-   const startTimer = () => {
-      const countdownDate = new Date('May 30, 2024 00:00:00').getTime();
-
-      interval = setInterval(() => {
-         const now = new Date().getTime();
-         const distance = countdownDate - now;
-         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-         const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-         if (distance < 0) {
-            //stop timer
-            clearInterval(interval.current);
-         } else {
-            // update timer
-            setTimerDays(String(days));
-            setTimerHours(String(hours));
-            setTimerMinutes(String(minutes));
-            setTimerSeconds(String(seconds));
-         }
-      }, 1000)
-   };
-
    useEffect(() => {
-      startTimer();
-      return () => {
-         clearInterval(interval.current);
-      }
-   }, [])
+      const startTimer = () => {
+         const countdownDate = new Date('August 24, 2024 00:00:00').getTime();
+
+         const interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (distance < 0) {
+               // Stop timer
+               clearInterval(interval);
+            } else {
+               // Update timer
+               setTimerDays(String(days));
+               setTimerHours(String(hours));
+               setTimerMinutes(String(minutes));
+               setTimerSeconds(String(seconds));
+            }
+         }, 1000);
+
+         return () => clearInterval(interval); // Clear interval when the component unmounts
+      };
+
+      const cleanupInterval = startTimer(); // Start the timer and store the returned interval ID for cleanup
+
+      return () => cleanupInterval(); // Return the cleanup function
+   }, []);
 
    return (
-      <div className={style.timer}>
-         <div>
-            <p>{timerDays}</p>
-            <p><small>Дней</small></p>
-         </div>
-         <span>:</span>
-         <div>
-            <p>{timerHours}</p>
-            <p><small>Часов</small></p>
-         </div>
-         <span>:</span>
-         <div>
-            <p>{timerMinutes}</p>
-            <p><small>Минут</small></p>
-         </div>
-         <span>:</span>
-         <div>
-            <p>{timerSeconds}</p>
-            <p><small>Секунд</small></p>
+      <div className={style.container}>
+         <div className={style.timer}>
+            <div>
+               <p>{timerDays}</p>
+               <p><small>Дней</small></p>
+            </div>
+            <span>:</span>
+            <div>
+               <p>{timerHours}</p>
+               <p><small>Часов</small></p>
+            </div>
+            <span>:</span>
+            <div>
+               <p>{timerMinutes}</p>
+               <p><small>Минут</small></p>
+            </div>
+            <span>:</span>
+            <div>
+               <p>{timerSeconds}</p>
+               <p><small>Секунд</small></p>
+            </div>
          </div>
       </div>
+
    )
 }
 
